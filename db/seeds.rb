d0dafbba2cc8d04c976a548d5afed26f77e116a3
weyewe <w.yunnal@gmail.com>
2011-05-17 20:08:23 +0700

    added seeds + setup basic CanCan

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
-Category.create :name => "A2B"
-Category.create :name => "Dumptruck"
-Category.create :name => "Support&Sarana"
+
+User.create :email => "w.yunnal@gmail.com" , :password => "willy1234", 
+  :password_confirmation => "willy1234", :role => "admin"
+  
+User.create :email => "sudirmanutomo@yahoo.com", :password => "dirman1234",
+  :password_confirmation => "dirman1234", :role => "director"
+
+User.create :email => "trie.entry@gmail.com" , :password => "trie1234",
+  :password_confirmation => "trie1234", :role => "entry"
 
