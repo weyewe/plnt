c22a480185be7379ed146ca5b80c0f124f331368
Coder <weyewe@Willy-Yunnals-MacBook-Pro.local>
2011-05-17 07:26:26 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plnt::Application.config.secret_token = '9859aa3256156aad180c211c0d98acb88cea1b590b00226516a5a25a7e1cc1f0a717f400bb4054ed356e96370fc2cdc2607921d946a5d59b2728e9fd5d621b12'
